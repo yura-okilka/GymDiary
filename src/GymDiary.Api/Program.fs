10134e82ebe227f5ba93c72786961321250050d8
Yura Okilka <yura.okilka@gmail.com>
2024-02-17 21:28:23 +0200

    Inline configuration.

--- src/GymDiary.Api/Program.fs
+++ src/GymDiary.Api/Program.fs
@@ -1,6 +1,5 @@
 namespace GymDiary.Api
 
-open System
 open System.Text.Json
 open System.Text.Json.Serialization
 
@@ -22,82 +21,58 @@ open Validus
 
 module Program =
 
-    let configureSettings (builder: IConfigurationBuilder) (environment: string) =
-        builder
-            .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile("appsettings.json")
-            .AddJsonFile($"appsettings.{environment}.json", optional = true)
-            .AddJsonFile("appsettings.local.json", optional = true)
-            .AddEnvironmentVariables("DOTNET_")
-            .AddEnvironmentVariables("ASPNETCORE_")
-            .AddEnvironmentVariables()
-        |> ignore
-
-    let configureAppConfiguration (context: WebHostBuilderContext) (builder: IConfigurationBuilder) (args: string[]) =
-        let env = context.HostingEnvironment.EnvironmentName
-        builder.Sources.Clear()
-        configureSettings builder env
-
-        if args.Length > 0 then
-            builder.AddCommandLine args |> ignore
-
-    let configureLogging (builder: ILoggingBuilder) = builder.AddConsole().AddDebug() |> ignore
-
-    let configureServices (services: IServiceCollection) =
-        PersistenceModule.configure () // MongoDB conventions must be configured before using MongoClient in the composition root.
-
-        services.AddGiraffe() |> ignore
-
-        // Configure JSON serialization
-        let jsonOptions = JsonSerializerOptions()
-        jsonOptions.PropertyNamingPolicy <- JsonNamingPolicy.CamelCase
-
-        jsonOptions.Converters.Add(
-            JsonFSharpConverter(
-                unionTagName = "type",
-                unionEncoding =
-                    (JsonUnionEncoding.InternalTag
-                     ||| JsonUnionEncoding.NamedFields
-                     ||| JsonUnionEncoding.UnwrapOption
-                     ||| JsonUnionEncoding.UnwrapSingleCaseUnions
-                     ||| JsonUnionEncoding.AllowUnorderedTag)
-            )
-        )
-
-        services.AddSingleton(jsonOptions) |> ignore
-        services.AddSingleton<Json.ISerializer, SystemTextJson.Serializer>() |> ignore
-
-    let configureApp (context: WebHostBuilderContext) (app: IApplicationBuilder) =
-        let env = context.HostingEnvironment.EnvironmentName
-
-        match env with
-        | "Development" -> app.UseDeveloperExceptionPage() |> ignore
-        | _ -> app.UseGiraffeErrorHandler(ErrorHandlers.unknownError) |> ignore
-
-        let settings = context.Configuration.Get<AppSettings>()
-
-        match AppSettings.validate settings with
-        | Error errors ->
-            errors
-            |> ValidationErrors.toList
-            |> String.concat "; "
-            |> fun msg -> failwith $"Invalid settings: %s{msg}"
-        | Ok _ -> ()
-
-        let root = (settings, app.ApplicationServices) ||> Trunk.compose |> CompositionRoot.compose
-
-        app.UseGiraffe(Router.webApp root)
-
     [<EntryPoint>]
     let main args =
         Host
             .CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(fun webHostBuilder ->
                 webHostBuilder
-                    .ConfigureAppConfiguration(fun context builder -> configureAppConfiguration context builder args)
-                    .ConfigureLogging(configureLogging)
-                    .ConfigureServices(configureServices)
-                    .Configure(configureApp)
+                    .ConfigureLogging(fun b -> b.AddConsole().AddDebug() |> ignore)
+                    .ConfigureServices(fun services ->
+                        // MongoDB conventions must be configured before using MongoClient in the composition root.
+                        PersistenceModule.configure ()
+
+                        services.AddGiraffe() |> ignore
+
+                        // Configure JSON serialization
+                        let jsonOptions = JsonSerializerOptions()
+                        jsonOptions.PropertyNamingPolicy <- JsonNamingPolicy.CamelCase
+
+                        jsonOptions.Converters.Add(
+                            JsonFSharpConverter(
+                                unionTagName = "type",
+                                unionEncoding =
+                                    (JsonUnionEncoding.InternalTag
+                                     ||| JsonUnionEncoding.NamedFields
+                                     ||| JsonUnionEncoding.UnwrapOption
+                                     ||| JsonUnionEncoding.UnwrapSingleCaseUnions
+                                     ||| JsonUnionEncoding.AllowUnorderedTag)
+                            )
+                        )
+
+                        services.AddSingleton(jsonOptions) |> ignore
+                        services.AddSingleton<Json.ISerializer, SystemTextJson.Serializer>() |> ignore)
+
+                    .Configure(fun context app ->
+                        let env = context.HostingEnvironment.EnvironmentName
+
+                        match env with
+                        | "Development" -> app.UseDeveloperExceptionPage() |> ignore
+                        | _ -> app.UseGiraffeErrorHandler(ErrorHandlers.unknownError) |> ignore
+
+                        let settings = context.Configuration.Get<AppSettings>()
+
+                        match AppSettings.validate settings with
+                        | Error errors ->
+                            errors
+                            |> ValidationErrors.toList
+                            |> String.concat "; "
+                            |> fun msg -> failwith $"Invalid settings: %s{msg}"
+                        | Ok _ -> ()
+
+                        let root = (settings, app.ApplicationServices) ||> Trunk.compose |> CompositionRoot.compose
+
+                        app.UseGiraffe(Router.webApp root))
                 |> ignore)
             .Build()
             .Run()
