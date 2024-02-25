5000298755be44d18e83bdbe2eed77341a323c19
Yura Okilka <yura.okilka@gmail.com>
2024-02-25 19:14:04 +0200

    Rename tests.

--- tests/GymDiary.Api.SystemTests/Features/Exercise_category_feature.cs
+++ tests/GymDiary.Api.SystemTests/Features/Exercise_category_feature.cs
@@ -5,7 +5,7 @@ namespace GymDiary.Api.SystemTests.Features;
 public class Exercise_category_feature : FeatureFixture
 {
     [Scenario]
-    public async Task When_create_exercise_category_then_response_should_be_successful()
+    public async Task Create_exercise_category()
     {
         await Runner
             .WithContext<ExerciseCategoryContext>()
