3e4c082ce961006e5d7a9b7f7d7d90efd13b6f8c
Yura Okilka <yura.okilka@gmail.com>
2024-08-24 12:31:02 +0300

    Rename IdProvider.

--- src/GymDiary.Persistence/ObjectIdProvider.fs
+++ src/GymDiary.Persistence/ObjectIdProvider.fs
@@ -4,9 +4,9 @@ open GymDiary.Core.Domain.CommonTypes
 open GymDiary.Core.Persistence
 open MongoDB.Bson
 
-type IdProvider() =
+type ObjectIdProvider() =
     interface IIdProvider with
-        member _.GenerateId<'T>() = ObjectId.GenerateNewId().ToString() |> Id.create<'T>
+        member _.GenerateId() = ObjectId.GenerateNewId().ToString() |> Id.create<'T>
 
         member _.TryParse value =
             match ObjectId.TryParse value with
