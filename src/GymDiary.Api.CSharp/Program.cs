a5549e4581a04c118bbb18480b118d5a13fb8db6
Yura Okilka <yura.okilka@gmail.com>
2025-05-16 21:52:31 +0300

    Add SwaggerUI.

--- src/GymDiary.Api.CSharp/Program.cs
+++ src/GymDiary.Api.CSharp/Program.cs
@@ -1,15 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "v1"));
 }
 
 app.UseHttpsRedirection();
@@ -23,11 +21,11 @@ app.MapGet(
         "/weatherforecast",
         () =>
         {
-            var forecast = Enumerable.Range(1, 5).Select(
+            var forecast = Enumerable.Range(start: 1, count: 5).Select(
                     index =>
                         new WeatherForecast(
                             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            Random.Shared.Next(-20, 55),
+                            Random.Shared.Next(minValue: -20, maxValue: 55),
                             summaries[Random.Shared.Next(summaries.Length)]
                         )
                 )
