f10066052771a88e6b6b432a8169e848361af3e7
Yura Okilka <yura.okilka@gmail.com>
2025-05-28 17:59:52 +0300

    Add ConfigurationExtensions.

---
+++ src/GymDiary.Infrastructure/Configuration/ConfigurationExtensions.fs
@@ -0,0 +1,26 @@
+namespace GymDiary.Infrastructure.Configuration
+
+open System
+open System.Runtime.CompilerServices
+open Microsoft.Extensions.Configuration
+
+[<Extension>]
+type ConfigurationExtensions() =
+
+    [<Extension>]
+    static member GetOrThrow(configuration: IConfiguration) : 'T =
+        match configuration.Get<'T>() with
+        | null -> raise (InvalidOperationException($"Cannot bind configuration to type {typeof<'T>.Name}"))
+        | value -> value
+
+    [<Extension>]
+    static member GetValueOrThrow (configuration: IConfiguration) (name: string) : 'T =
+        match configuration.GetValue<'T>(name) with
+        | null -> raise (InvalidOperationException($"Value {name} was not found"))
+        | value -> value
+
+    [<Extension>]
+    static member GetConnectionStringOrThrow (configuration: IConfiguration) (name: string) : string =
+        match configuration.GetConnectionString(name) with
+        | null -> raise (InvalidOperationException($"Connection string {name} was not found"))
+        | value -> value
