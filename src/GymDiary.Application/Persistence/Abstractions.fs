7bb59c2f2ec8db11ac989130c16a6d454d91b2b6
Yura Okilka <yura.okilka@gmail.com>
2026-06-21 17:26:59 +0300

    Rename repository id type parameter to 'TEntityId

--- src/GymDiary.Application/Persistence/Abstractions.fs
+++ src/GymDiary.Application/Persistence/Abstractions.fs
@@ -10,18 +10,18 @@ open GymDiary.Domain.Users
 type UpdateEntityError = EntityNotFound of entity: string * id: string
 type UpdateEntityResult = Result<unit, UpdateEntityError>
 
-type IEntityRepository<'TEntity, 'TId> =
+type IEntityRepository<'TEntity, 'TEntityId> =
     abstract member Create: 'TEntity -> Task<unit>
     abstract member Update: 'TEntity -> Task<UpdateEntityResult>
-    abstract member Delete: 'TId -> Task<unit>
-    abstract member Get: 'TId -> Task<'TEntity option>
-    abstract member ExistsWithId: 'TId -> Task<bool>
+    abstract member Delete: 'TEntityId -> Task<unit>
+    abstract member Get: 'TEntityId -> Task<'TEntity option>
+    abstract member ExistsWithId: 'TEntityId -> Task<bool>
 
-type IOwnedEntityRepository<'TEntity, 'TId> =
-    inherit IEntityRepository<'TEntity, 'TId>
-    abstract member GetOneByOwner: 'TId -> UserId -> Task<'TEntity option>
+type IOwnedEntityRepository<'TEntity, 'TEntityId> =
+    inherit IEntityRepository<'TEntity, 'TEntityId>
+    abstract member GetOneByOwner: 'TEntityId -> UserId -> Task<'TEntity option>
     abstract member GetAllByOwner: UserId -> Task<'TEntity list>
-    abstract member DeleteByOwner: 'TId -> UserId -> Task<bool>
+    abstract member DeleteByOwner: 'TEntityId -> UserId -> Task<bool>
 
 type IUserRepository =
     inherit IEntityRepository<User, UserId>
